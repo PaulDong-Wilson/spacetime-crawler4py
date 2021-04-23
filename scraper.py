077f2cc3e395dee7d5c03024ab1aacd02305a8ae
Nathanael Scudder <nathanael478@gmail.com>
2021-04-22 23:25:47 -0700

    Added More Banned Extensions + Added ss and scm to scraper.py's extension ban list

--- scraper.py
+++ scraper.py
@@ -70,7 +70,7 @@ def is_valid(url):
             r".*\.(css|js|java|r|py|c|m|bmp|gif|jpe?g|ico"
             + r"|png|tiff?|mid|mp2|mp3|mp4"
             + r"|wav|avi|mov|mpeg|ram|m4v|mkv|odc|ogg|ogv|pdf"
-            + r"|ps|eps|tex|ppt|pptx|ppsx|txt|doc|docx|xls|xlsx|names"
+            + r"|ps|eps|tex|ppt|pptx|ppsx|ss|scm|txt|doc|docx|xls|xlsx|names"
             + r"|data|dat|exe|bz2|tar|msi|bin|7z|psd|z|dmg|iso"
             + r"|epub|dll|cnf|tgz|sha1"
             + r"|thmx|mso|arff|rtf|jar|csv"
